dd0c7ba2fc1b7c38a93af342a342d46298431f67
EnTaro <apan.kirill@yandex.ru>
2019-12-25 20:19:48 +0300

    Первый вариант поля с точками.

---
+++ TEC_Game2.0/Assets/Scripts/MapScroller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Vector3 = UnityEngine.Vector3;
+
+public class MapScroller : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+    public void OnDrag(PointerEventData eventData)
+    {
+        GameObject map = GameObject.Find("Grid");
+        Vector3 delta = Camera.main.ScreenToViewportPoint(eventData.delta);
+        map.transform.position += new Vector3(delta.x*15, delta.y*15, 0);
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("Start");
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("End");
+    }
+}
