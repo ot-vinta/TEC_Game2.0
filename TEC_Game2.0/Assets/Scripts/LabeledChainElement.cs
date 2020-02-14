59eee599078d7982fcccaa8d16046608c2a23384
MPozd <47981972+MPozd@users.noreply.github.com>
2020-02-14 19:25:50 +0300

    Big-ass labels are now attached to the world
    Теперь подписи добавляются, и даже привязаны к миру, а не экрану. Вот только они дохера огромны.

--- TEC_Game2.0/Assets/Scripts/LabeledChainElement.cs
+++ TEC_Game2.0/Assets/Scripts/LabeledChainElement.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 using UnityEngine.UI;
 
 namespace Assets.Scripts
@@ -17,8 +18,20 @@ namespace Assets.Scripts
             //
             //this.label.transform.position = new Vector3Int(position.x, position.y + 1, position.z);
 
-            Text template = GameObject.Find("Labels").GetComponentInChildren<Text>();
-            this.label = UnityEngine.Object.Instantiate(template, new Vector3Int(position.x, position.y + 1, position.z), new Quaternion(0,0,0,0));
+            GameObject labels = GameObject.Find("Labels");
+            Text template = labels.GetComponentInChildren<Text>();
+            //this.label = UnityEngine.Object.Instantiate(template, new Vector3Int(position.x, position.y + 1, position.z), new Quaternion(0,0,0,0));
+
+
+            Tilemap tilemap = GameObject.FindObjectOfType<Tilemap>();
+            Vector3 worldPosition = tilemap.CellToWorld(position);
+            Vector3Int worldPositionInt = new Vector3Int((int)worldPosition.x, (int)worldPosition.y, (int)worldPosition.z);
+
+            this.label = UnityEngine.Object.Instantiate(template, worldPositionInt, Quaternion.identity, labels.transform);
+            //this.label = UnityEngine.Object.Instantiate(template, position, Quaternion.identity, tilemap.transform);
+
+            //this.labe
+
             this.label.text = "Элемент цепи";
             this.label.enabled = true;
 
