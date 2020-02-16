9ffb12ef2538b0da596375ed17a5d5cf90a90ea2
MPozd <47981972+MPozd@users.noreply.github.com>
2020-02-16 19:29:47 +0300

    Попытка настроить переименование
    Добавлен скрипт для подписи. Проблема с вызовом диалога с другой сцены. Буду думать

---
+++ TEC_Game2.0/Assets/Scripts/Label.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Label : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            UIInputBox dialog = new UIInputBox();
+            //Text 
+            Debug.Log("Нажата правая кнопка мыши на подпись");
+            dialog.SetOnClickListener(message =>
+            {
+                dialog.title.text = message;
+                return true;
+            });
+            dialog.ShowDialog("Проверка вызова из PlayLevelControls");
+        }
+    }
+}
