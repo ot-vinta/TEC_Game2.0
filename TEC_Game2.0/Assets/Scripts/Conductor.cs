689e00fadb50d7af4d29dc005f6a396e6b01a892
MPozd <47981972+MPozd@users.noreply.github.com>
2020-02-23 11:31:10 +0300

    Outstanding move

--- TEC_Game2.0/Assets/Scripts/Conductor.cs
+++ TEC_Game2.0/Assets/Scripts/Conductor.cs
@@ -12,6 +12,7 @@ namespace Assets.Scripts
         public Conductor(Vector3Int position, int angle) : base(position, angle)
         {
             this.label.text = "Проводимость";
+            //this.label
         }
         public Conductor(Vector3Int position, int angle, string name) : base(position, angle, name)
         {
