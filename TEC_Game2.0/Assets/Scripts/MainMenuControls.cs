3ecd7428914efee1ff6ecbed4099395dad713c69
Limriss <apan.kiril@yandex.ru>
2020-03-22 18:26:04 +0300

    Description

--- TEC_Game2.0/Assets/Scripts/MainMenuControls.cs
+++ TEC_Game2.0/Assets/Scripts/MainMenuControls.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuControls : MonoBehaviour
-{
+{	
+	UIList dialogList;
+	
+	void Start()
+    {
+        dialogList = new UIList();
+    }
 
     public void StartPressed()
     {
@@ -26,7 +32,24 @@ public class MainMenuControls : MonoBehaviour
 
     public void HelpPressed()
     {
-        //TO DO
+        var alarmShit = new string[16];
+		alarmShit[0] = "Программа TEC 2.0";
+		alarmShit[1] = "";
+		alarmShit[2] = "Целью программы является возможность";
+		alarmShit[3] = "продемонстрировать процесс упрощения схем с";
+		alarmShit[4] = "использованием нуллатора/норатора";
+		alarmShit[5] = "";
+		alarmShit[6] = "Авторы:";
+		alarmShit[7] = "Поздняков Михаил Владимирович";
+		alarmShit[8] = "Калашников Иван Геннадьевич";
+		alarmShit[9] = "Апанасович Кирилл Сергеевич";
+		alarmShit[10] = "Аксенов Владимир Алексеевич";
+		alarmShit[11] = "";
+		alarmShit[12] = "Руководитель";
+		alarmShit[13] = "Горшков Константин Сергеевич";
+		alarmShit[14] = "";
+		alarmShit[15] = "ИТМО 2020";
+		dialogList.ShowDialog(alarmShit);
     }
 }
 
