637d13ac1a51d4f755a4a8d26293835552d4acba
MPozd <47981972+MPozd@users.noreply.github.com>
2020-04-09 19:50:17 +0300

    Added exit button

---
+++ TEC_Game2.0/Assets/Scripts/ExitButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExitButton : MonoBehaviour
+{
+    Button button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = this.gameObject.GetComponent<Button>();
+        button.onClick.AddListener(delegate { Destroy(gameObject.GetComponentInParent<Canvas>()); } );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
